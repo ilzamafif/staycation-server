895d29bdab987d2594b3528ccfd61a6fcddd0665
Ilzam afif <ilzamafif27@gmail.com>
2023-04-01 17:51:03 +0700

    adding activity, feature, item model

---
+++ models/Item.js
@@ -0,0 +1,42 @@
+const mongoose = require('mongoose');
+const { ObjectId } = mongoose.Schema;
+
+new itemSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    country: {
+        type: String,
+        default: "Indonesia"
+    },
+    city: {
+        type: String,
+        required: true
+    },
+    IsPopular: {
+        type: Boolean,
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    imageId: {
+        type: ObjectId,
+        ref: "Image"
+    },
+    featureId: {
+        type: ObjectId,
+        ref: "Feature"
+    },
+    activityId: {
+        type: ObjectId,
+        ref: "Activity"
+    }
+})
+
+module.exports = mongoose.model('Item', itemSchema);
