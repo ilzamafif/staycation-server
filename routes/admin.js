1fe53839f49feaea9c88b6232605287babbc11ed
Ilzam afif <ilzamafif27@gmail.com>
2023-04-02 12:01:25 +0700

    adding view category admin

--- routes/admin.js
+++ routes/admin.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 const adminController = require("../controllers/adminController");
 
 router.get("/dashboard", adminController.viewDashboard);
+router.get("/category", adminController.viewCategory);
 
 module.exports = router;
