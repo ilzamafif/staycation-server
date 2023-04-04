78bd1181b1e30199b71a2e21b53f6189da64448e
Ilzam afif <ilzamafif27@gmail.com>
2023-04-04 13:54:27 +0700

    adding fungsionality show activity

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -343,12 +343,14 @@ module.exports = {
       const alertStatus = req.flash("alertStatus");
       const alert = { message: alertMessage, status: alertStatus };
       const feature = await Feature.find({ itemId: itemId });
+      const activity = await Activity.find({ itemId: itemId });
 
       res.render("admin/item/detail_item/view_detail_item", {
         title: "Staycation | Detail Item",
         alert,
         itemId,
         feature,
+        activity,
       });
     } catch (error) {
       req.flash("alertMessage", `${error.message}`);
