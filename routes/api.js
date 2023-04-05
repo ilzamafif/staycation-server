23a1a4f0eda825632029d8af5d24df4b71664ce4
Ilzam afif <ilzamafif27@gmail.com>
2023-04-05 11:16:33 +0700

    create detail page API

--- routes/api.js
+++ routes/api.js
@@ -3,4 +3,5 @@ const apiController = require("../controllers/apiController");
 // const { uploadSingle } = require('../middlewares/multer');
 
 router.get("/landing-page", apiController.landingPage);
+router.get("/detail-page/:id", apiController.detailPage);
 module.exports = router;
